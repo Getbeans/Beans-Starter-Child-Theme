ca749084c3e3bd467fa87952a0bb7c622a51424f
ThierryA <thierry@getbeans.io>
2018-01-24 11:46:51 +0100

    Added tests logic to build and sample tests

---
+++ tests/phpunit/unit/sample/unitSample.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Tests sample
+ *
+ * @package Beans\Child\Tests\Unit\Sample
+ *
+ * @since   1.5.0
+ */
+
+namespace Beans\Child\Tests\Unit\Sample;
+
+use Beans\Child\Tests\Unit\Test_Case;
+use Brain\Monkey\Functions;
+use Brain\Monkey\Actions;
+
+/**
+ * Class Tests_unitSample
+ *
+ * @package Beans\Child\Tests\Unit\Sample
+ * @group   unit-tests
+ * @group   sample
+ */
+class Tests_UnitSample extends Test_Case {
+
+	/**
+	 * Sample test.  Will be replaced by real tests.
+	 */
+	public function test_sample() {
+		$this->assertTrue( true );
+	}
+}
